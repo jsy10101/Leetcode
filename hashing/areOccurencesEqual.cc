172e66b5a4dbd6e6480ce2a3414a72f359a8bf89
jsy10101 <jaskiratchawla580@gmail.com>
2024-05-24 00:15:12 -0400

    add areOccurencesEqual.cc

---
+++ hashing/areOccurencesEqual.cc
@@ -0,0 +1,19 @@
+// Time: O(n)
+// Space: O(k)
+//		  - where k = 26 in our case (letters come from english alphabet)
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        unordered_map<char, int> counts;
+        for (char c: s) {
+            counts[c]++;
+        }
+
+        unordered_set<int> frequencies;
+        for (auto [key, val]: counts) {
+            frequencies.insert(val);
+        }
+
+        return frequencies.size() == 1;
+    }
+};
