9d088ecd3939305ca416064ac4fbd16fedd4b4f0
jsy10101 <jaskiratchawla580@gmail.com>
2024-05-23 23:34:47 -0400

    add findLongestSubstring.cc

---
+++ hashing/findLongestSubstring.cc
@@ -0,0 +1,21 @@
+// Time: O(n)
+// Space: O(k)
+int findLongestSubstring(string s, int k) {
+    unordered_map<char, int> counts;
+    int left = 0, ans = 0;
+    
+    for (int right = 0; right < s.size(); right++) {
+        counts[s[right]]++;
+        while (counts.size() > k) {
+            counts[s[left]]--;
+            if (counts[s[left]] == 0) {
+                counts.erase(s[left]);
+            }
+            left++;
+        }
+        
+        ans = max(ans, right - left + 1);
+    }
+    
+    return ans;
+}
