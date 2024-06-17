0ccd9b765c242669e1486b77cc3b147c21080b62
jsy10101 <jaskiratchawla580@gmail.com>
2024-06-17 13:53:41 -0400

    add numIslands.cc

---
+++ trees_and_graphs/numIslands.cc
@@ -0,0 +1,43 @@
+// Time: O(m * n)
+class Solution {
+public:
+    int m;
+    int n;
+    vector<vector<char>> grid;
+    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    vector<vector<bool>> seen;
+    
+    int numIslands(vector<vector<char>>& grid) {
+        this->grid = grid;
+        m = grid.size();
+        n = grid[0].size();
+        seen = vector(m, vector<bool>(n, false));
+        
+        int ans = 0;
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (grid[row][col] == '1' && !seen[row][col]) {
+                    ans++;
+                    seen[row][col] = true;
+                    dfs(row, col);
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    void dfs(int row, int col) {
+        for (vector<int>& direction: directions) {
+            int nextRow = row + direction[0], nextCol = col + direction[1];
+            if (valid(nextRow, nextCol) && !seen[nextRow][nextCol]) {
+                seen[nextRow][nextCol] = true;
+                dfs(nextRow, nextCol);
+            }
+        }
+    }
+    
+    bool valid(int row, int col) {
+        return 0 <= row && row < m && 0 <= col && col < n && grid[row][col] == '1';
+    }
+};
