3877e344f2443f1e9bc0b98247be05bb336f17ab
jsy10101 <jaskiratchawla580@gmail.com>
2025-01-21 20:04:11 -0500

    add heaps/kClosestPointsOrigin.cc

---
+++ heaps/kClosestPointsOrigin.cc
@@ -0,0 +1,24 @@
+// Time: O(n * log k)
+// Space: O(k)
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, vector<int>>> heap;
+        
+        for(auto point: points) {
+            int distance = point[0] * point[0] + point[1] * point[1];
+            heap.push(pair(distance, point));
+            if (heap.size() > k) {
+                heap.pop();
+            }
+        }
+        
+        vector<vector<int>> ans;
+        while (!heap.empty()) {
+            ans.push_back(heap.top().second);
+            heap.pop();
+        }
+        
+        return ans;
+    }
+};
