2a091cf354615ff4f8b167888cc1afc0b295600d
jsy10101 <jaskiratchawla580@gmail.com>
2025-01-21 19:30:49 -0500

    add topKFrequent.cc

--- heaps/topKFrequent.cc
+++ heaps/topKFrequent.cc
@@ -1,24 +1,31 @@
 // Time: O(n * log k)
-// Space: O(k)
+// Space: O(n + k)
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> counts;
+
+        // O(n)
         for (int num: nums) {
             counts[num]++;
         }
-        
+
         priority_queue<pair<int, int>> heap;
+        // we have to maintain size k for heap;
+        // O(n * log k)
+        // at most size of heap is k
         for (auto [key, val]: counts) {
             heap.push({-val, key});
             if (heap.size() > k) {
                 heap.pop();
             }
         }
-        
+
         vector<int> ans;
+        // O(k * log k)
         while (heap.size() > 0) {
-            ans.push_back(heap.top().second); heap.pop();
+            ans.push_back(heap.top().second);
+            heap.pop();
         }
 
         return ans;
