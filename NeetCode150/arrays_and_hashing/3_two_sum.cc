3e4b6856e698b0d62ce658b925ec8a645e743347
jsy10101 <jaskiratchawla580@gmail.com>
2025-02-27 00:36:06 -0500

    add NeetCode150/arrays_and_hashing/3_two_sum.cc

---
+++ NeetCode150/arrays_and_hashing/3_two_sum.cc
@@ -0,0 +1,15 @@
+// Time: O(n)
+// Space: O(n)
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int, int> map;
+    
+            for (int i = 0; i < nums.size(); ++i) {
+                if (map.contains(target - nums[i])) return {map[target - nums[i]], i};
+                map[nums[i]] = i;
+            }
+    
+            return {-1, -1};
+        }
+    };
