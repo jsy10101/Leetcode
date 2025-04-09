017ff17793259c0cfd7b2d834401b3acce2e4d63
jsy10101 <jaskiratchawla580@gmail.com>
2025-04-09 14:06:17 -0400

    add NeetCode150/binary_search/searchMatrix.cc

--- NeetCode150/binary_search/searchMatrix.cc
+++ NeetCode150/binary_search/searchMatrix.cc
@@ -1,20 +1,54 @@
+// Time: O(log (m*n))
+// Space: O(1)
+// Algo intuition:
+//      we apply binary search two times to solve in O(log m + log n) = O(log (m*n)) time
+//      first we declare top and bottom vars equivalent to first and last row
+//      this is to identify the potential row where our target element can be
+//      if the target is bigger than the greatest element in row -> last element in row, we search in the bottom half and set top = mid + 1;
+//      else if target is smaller than the smallest element in the mid row -> first element, we search in the top half and set bottom = mid - 1;
+//      once we have the potential row, break
+//      then apply the usual binary search algo to find if the target is present or not
+//      EDGE CASE - if top > bottom, this cannot be possible and hence we can conclude that the element is not present in matrix
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int ROWS = matrix.size(), COLS = matrix[0].size();
+        int ROWS = matrix.size();
+        int COLS = matrix[0].size();
 
-        int l = 0, r = ROWS * COLS - 1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            int row = m / COLS, col = m % COLS;
+        int top = 0;
+        int bottom = ROWS - 1;
+
+        while (top <= bottom) {
+            int row = (top + bottom) / 2;
+
+            if (target > matrix[row][COLS - 1]) {
+                top = row + 1;
+            } else if (target < matrix[row][0]) {
+                bottom = row - 1;
+            } else {
+                break;
+            }
+        }
+
+        if (!(top <= bottom)) {
+            return false;
+        }
+
+        int row = (top + bottom) / 2;
+        int beg = 0;
+        int end = COLS - 1;
+
+        while (beg <= end) {
+            int col = (beg + end) / 2;
             if (target > matrix[row][col]) {
-                l = m + 1;
+                beg = col + 1;
             } else if (target < matrix[row][col]) {
-                r = m - 1;
+                end = col - 1;
             } else {
                 return true;
             }
         }
+
         return false;
     }
 };
