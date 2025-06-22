f23fedc07b80481351c054abc46ea49824e01b5d
jsy10101 <jaskiratchawla580@gmail.com>
2025-06-22 13:14:01 -0400

    add interviews/amazon/1_twoSum.cc

---
+++ interviews/amazon/1_twoSum.cc
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (map.contains(target - nums[i])) {
+                int complement = target - nums[i];
+                return {map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};
