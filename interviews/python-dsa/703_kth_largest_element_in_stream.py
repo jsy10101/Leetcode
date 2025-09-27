bac733a09fcaa8e82cbc82c00442e8a283c77b2f
jsy10101 <jaskiratchawla580@gmail.com>
2025-09-26 23:57:49 -0400

    add interviews/python-dsa/703_kth_largest_element_in_stream.py

---
+++ interviews/python-dsa/703_kth_largest_element_in_stream.py
@@ -0,0 +1,31 @@
+from typing import List
+import heapq
+
+
+class KthLargest:
+    """
+    Algorithm intuition:
+        - define a min heap to store k largest elements
+        - the top of heap will always return the k-th largest element
+    Time: O((m + n) * log k) <- m = initial num size, and n for add function
+    Space: O(k)
+    """
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap = []
+
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        if len(self.min_heap) < self.k or self.min_heap[0] < val:
+            heapq.heappush(self.min_heap, val)
+            if len(self.min_heap) > self.k:
+                heapq.heappop(self.min_heap)
+        return self.min_heap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
