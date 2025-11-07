e9b48be0ffaf49a0c6edd7376f46cb903f803141
jsy10101 <jaskiratchawla580@gmail.com>
2025-11-07 14:21:41 -0500

    add NeetcodeBlind75/binary_search/2_search_in_rotated_sorted_array.py

---
+++ NeetcodeBlind75/binary_search/2_search_in_rotated_sorted_array.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        Algorithm intuition:
+            - IDK
+        Time: O(log n)
+        Space: O(1)
+        """
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            # left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            # right sorted portion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
