2a2ac1ee9316dcc71552a5c2971dc7a36a09a499
jsy10101 <jaskiratchawla580@gmail.com>
2025-11-07 14:50:08 -0500

    add NeetcodeBlind75/linked_list/4_reorder_list.py

---
+++ NeetcodeBlind75/linked_list/4_reorder_list.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Algorithm intuition:
+            - use two pointer (fast and slow) to find middle of list
+                - start fast from head.next
+            - reverse the second half
+            - iterate through second list (reversed) and change pointers
+        Time: O(n)
+        Space: O(1)
+        """
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
